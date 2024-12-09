5a2275637488bad60fb0454c3abc67b46718e687
Sinan Can <sinan.can@eon.com>
2024-12-09 09:24:17 +0100

    remove android

--- lib/qr_bar_code_scanner_dialog_method_channel.dart
+++ lib/qr_bar_code_scanner_dialog_method_channel.dart
@@ -3,27 +3,24 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_code_scanner_plus/qr_code_scanner_plus.dart';
 
 import 'qr_bar_code_scanner_dialog_platform_interface.dart';
 
 /// An implementation of [QrBarCodeScannerDialogPlatform] that uses method channels.
-class MethodChannelQrBarCodeScannerDialog
-    extends QrBarCodeScannerDialogPlatform {
+class MethodChannelQrBarCodeScannerDialog extends QrBarCodeScannerDialogPlatform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
   final methodChannel = const MethodChannel('qr_bar_code_scanner_dialog');
 
   @override
   Future<String?> getPlatformVersion() async {
-    final version =
-        await methodChannel.invokeMethod<String>('getPlatformVersion');
+    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
     return version;
   }
 
   @override
-  void scanBarOrQrCode(
-      {BuildContext? context, required Function(String? code) onScanSuccess}) {
+  void scanBarOrQrCode({BuildContext? context, required Function(String? code) onScanSuccess}) {
     /// context is required to show alert in non-web platforms
     assert(context != null);
 
@@ -99,32 +96,29 @@ class _ScannerWidgetState extends State<ScannerWidget> {
             child: Stack(
               children: [
                 _buildQrView(context),
-               if (hasFlash) Positioned(
-                  bottom: 5,
-                  right: 5,
-                  child: CircleAvatar(
-                    backgroundColor:
-                        Theme.of(context).colorScheme.secondaryContainer,
-                    child: IconButton(
-                      color: Theme.of(context).colorScheme.onSecondaryContainer,
-                      onPressed: () async {
-                        if (controller != null) {
-                          await controller?.toggleFlash();
-                          isFlashOn =
-                              (await controller?.getFlashStatus()) ?? false;
-                          setState(() {
-                            debugPrint("isFlashOn: $isFlashOn");
-                          });
-                        }
-                      },
-                      icon: Icon(
-                        isFlashOn
-                            ? Icons.flashlight_on_outlined
-                            : Icons.flashlight_off_rounded,
+                if (hasFlash)
+                  Positioned(
+                    bottom: 5,
+                    right: 5,
+                    child: CircleAvatar(
+                      backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+                      child: IconButton(
+                        color: Theme.of(context).colorScheme.onSecondaryContainer,
+                        onPressed: () async {
+                          if (controller != null) {
+                            await controller?.toggleFlash();
+                            isFlashOn = (await controller?.getFlashStatus()) ?? false;
+                            setState(() {
+                              debugPrint("isFlashOn: $isFlashOn");
+                            });
+                          }
+                        },
+                        icon: Icon(
+                          isFlashOn ? Icons.flashlight_on_outlined : Icons.flashlight_off_rounded,
+                        ),
                       ),
                     ),
                   ),
-                ),
               ],
             ),
           ),
@@ -143,8 +137,7 @@ class _ScannerWidgetState extends State<ScannerWidget> {
   }
 
   Widget _buildQrView(BuildContext context) {
-    double smallestDimension = min(
-        MediaQuery.of(context).size.width, MediaQuery.of(context).size.height);
+    double smallestDimension = min(MediaQuery.of(context).size.width, MediaQuery.of(context).size.height);
 
     smallestDimension = min(smallestDimension, 550);
 
@@ -168,7 +161,6 @@ class _ScannerWidgetState extends State<ScannerWidget> {
     this.controller = controller;
 
     var result = await controller.getSystemFeatures();
-
 
     controller.scannedDataStream.listen((Barcode scanData) async {
       if (!isScanned) {
